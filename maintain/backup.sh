fa061a77694c5f8bdbd7fec399eba7f548fc587a
gab2 <gabmueller@hotmail.com>
2024-03-22 08:48:26 +0100

    [btrfs/snaps] merge confl fix and child_vol corrected

--- maintain/backup.sh
+++ maintain/backup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-source /home/gab2/bin/mountLuksDev.sh # >> /dev/null
+source /home/gab2/bin/mountLuksDev.sh >> /dev/null
 cd /home
 dt_str=$(date +%Y%m%d)
 parent_vol=$(btrs list ./ | grep -v "@$dt_str" | grep "@20\(2[4-9]\|[3-9][0-9]\)" | tail -1 | cut -d' ' -f9 )
@@ -8,7 +8,7 @@ if [ -z "$parent_vol" ]; then
     return -1
 fi
 echo Found parent volume /home/"$parent_vol"
-child_vol=$(btrs list ./ | grep "$dt_str" | tail -1 | cut -d' ' -f9 | sed "s/\(.\+\)/\/\1/g" | grep ".\+" || btrs snapshot -r /home "@$dt_str" | \
+child_vol=$(btrs list ./ | grep "/@$dt_str" | tail -1 | cut -d' ' -f9 | sed "s/\(.\+\)/\/\1/g" | grep ".\+" || btrs snapshot -r /home "@$dt_str" | \
     sed "s/.\+\.\(\/\@20\(2[4-9]\|[3-9][0-9]\)\(0[1-9]\|1[0-2]\)\(0[1-9]\|[12][0-9]\|3[01]\)\).\+/\1/g" )
 # child_vol=$(echo "/@$(date +%Y%m%d )" | sed "s/.\+\.\(\/\@2024\(0[1-9]\|1[0-2]\)\)\(0[1-9]\|[12][0-9]\|3[01]\).\+/\1/g" )
 echo and new child volume /home"$child_vol" created
