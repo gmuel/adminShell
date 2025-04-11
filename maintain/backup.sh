869b1820ac2af1f8ac814ee12f73f33f5ff0b415
gab2 <gabmueller@hotmail.com>
2025-04-11 11:03:31 +0200

    [luks/mount] expanded backup: new checks for device and parent vols, enhanced reporting/status codes

--- maintain/backup.sh
+++ maintain/backup.sh
@@ -1,16 +1,41 @@
 #!/bin/bash
+vol_str=$1
+if [[ "$vol_str" == "-h" || "$vol_str" == "--help" ]]; then
+    cat << EOH
+$0 [options] [arg] - create backup arg/@date_string for any mount btrfs fs
+        
+        options: -h/--help print this message
+        
+        arg - any btrfs mount point (defaults to /home/ )
+        exit codes:
+            0  - backup succeeded
+            1  - no mount point
+            -1 - no matching source device or parent volume found
+            -2 - decryption/mount of destination device failed
+
+EOH
+    exit 0
+fi
 source /home/gab2/bin/mountLuksDev.sh >> /dev/null
-cd /home
+[ -z "$vol_str" ] && vol_str=/home
+len=${#vol_str}
+[[ "${vol_str:$((len-1))}" != "/" ]] && vol_str="$vol_str/"
+if [ ! -d $vol_str ]; then
+    echo no such mount point $vol_str
+    exit 1
+fi
+cd $vol_str || 
 dt_str=$(date +%Y%m%d)
 parent_vol=$(btrs list ./ | grep -v "@$dt_str" | grep "@20\(2[4-9]\|[3-9][0-9]\)" | tail -1 | cut -d' ' -f9 )
-if [ -z "$parent_vol" ]; then
-    echo "No suitable subvol found for parent in /home"
-    return -1
+if [[ "$?" != "0" || -z "$parent_vol" ]]; then
+    echo "No suitable subvol found for parent in $vol_str"
+    exit -1
 fi
-echo Found parent volume /home/"$parent_vol"
-child_vol=$(btrs list ./ | grep "@$dt_str" | tail -1 | cut -d' ' -f9 | sed "s/\(.\+\)/\/\1/g" | grep ".\+" || btrs snapshot -r /home "@$dt_str" | sed "s/.\+\.\(\/\@20\(2[4-9]\|[3-9][0-9]\)\(0[1-9]\|1[0-2]\)\(0[1-9]\|[12][0-9]\|3[01]\)\).\+/\1/g" )
+echo Found parent volume "$vol_str$parent_vol"
+child_vol=$(btrs list ./ | grep "@$dt_str" | tail -1 | cut -d' ' -f9 | sed "s/\(.\+\)/\/\1/g" | grep ".\+" || btrs snapshot -r $vol_str "@$dt_str" \
+    | sed "s/.\+\.\(\/\@20\(2[4-9]\|[3-9][0-9]\)\(0[1-9]\|1[0-2]\)\(0[1-9]\|[12][0-9]\|3[01]\)\).\+/\1/g" )
 # child_vol=$(echo "/@$(date +%Y%m%d )" | sed "s/.\+\.\(\/\@2024\(0[1-9]\|1[0-2]\)\)\(0[1-9]\|[12][0-9]\|3[01]\).\+/\1/g" )
-echo "and new child volume /home$child_vol created"
+echo "and new child volume $vol_str$child_vol created"
 backvol=
 if inxi -M | grep XPS; then
     backvol=2
@@ -32,11 +57,17 @@ if [ ! -z "$sbvl" ]; then
     if [ "$?" -eq "0" ]; then
         echo backup subvol mounted
         dr=/media/gab2/$UUID
+        chl_vol="$vol_str$child_vol"
+        prn_vol="$vol_str$parent_vol"
+        fl=
         if btrs list $dr | grep "$parent_vol"; then
-            btr send -p "/home/$parent_vol" "/home$child_vol" | btr receive $dr && \
-                echo "Child vol: \"/home$child_vol\" of parent vol: \"/home/$parent_vol\" sent to '$dr'"
+            btr send -p "$prn_vol" "$chl_vol" | btr receive $dr && \
+                echo "Child vol: \"$chl_vol\" of parent vol: \"$prn_vol\" sent to '$dr'" || \
+                    fl=0
         fi
-        umountLuksDev $dvc gab2
+        [ -z "$fl" ] && umountLuksDev $dvc gab2 || echo "backup failed - check device $dvc for issues"
+    else
+        echo luks mount failed for $dvc and subvol $sbvl
+        exit -2
     fi
 fi
-
