4e32d5f119b17f806fa68fe27de3c9208246a0d9
Gabriel Mueller <gabmueller@hotmail.com>
2025-04-14 22:21:44 +0200

    [luks/mount] new checks for already crypt-mounted devices, minor correction, checks for parent and child vol expanded

--- maintain/backup.sh
+++ maintain/backup.sh
@@ -34,6 +34,7 @@ fi
 echo Found parent volume "$vol_str$parent_vol"
 child_vol=$(btrs list ./ | grep "@$dt_str" | tail -1 | cut -d' ' -f9 | sed "s/\/\?\(.\+\)/\/\1/g" | grep ".\+" || btrs snapshot -r $vol_str "@$dt_str" \
     | sed "s/.\+\.\(\@20\(2[4-9]\|[3-9][0-9]\)\(0[1-9]\|1[0-2]\)\(0[1-9]\|[12][0-9]\|3[01]\)\).\+/\1/g" )
+child_vol=$(echo $child_vol | sed "s/\/\(.\+\)/\1/g" )
 # child_vol=$(echo "/@$(date +%Y%m%d )" | sed "s/.\+\.\(\/\@2024\(0[1-9]\|1[0-2]\)\)\(0[1-9]\|[12][0-9]\|3[01]\).\+/\1/g" )
 echo "and new child volume $vol_str$child_vol created"
 backvol=
@@ -53,21 +54,27 @@ sbvl="$(grep $UUID /home/gab2/bin/backup.map | cut -d' ' -f2 )$backvol"
 echo backup subvol found: $sbvl
 if [ ! -z "$sbvl" ]; then 
     dvc=$(echo $dvc | sed "s/\/dev\///g" )
-    mountLuksDev $dvc gab2 $sbvl
-    if [ "$?" -eq "0" ]; then
-        echo backup subvol mounted
-        dr=/media/gab2/$UUID
-        chl_vol="$vol_str$child_vol"
-        prn_vol="$vol_str$parent_vol"
-        fl=
-        if btrs list $dr | grep "$parent_vol"; then
-            btr send -p "$prn_vol" "$chl_vol" | btr receive $dr && \
-                echo "Child vol: \"$chl_vol\" of parent vol: \"$prn_vol\" sent to '$dr'" || \
-                    fl=0
-        fi
-        [ -z "$fl" ] && umountLuksDev $dvc gab2 || echo "backup failed - check device $dvc for issues"
-    else
+    if [ -z "$(mount | grep $dvc )" ]; then
+        mountLuksDev $dvc gab2 $sbvl
+    fi
+    if [ -z "$(mount | grep $dvc | grep $UUID )" ]; then
         echo luks mount failed for $dvc and subvol $sbvl
         exit -2
     fi
+    echo backup subvol mounted
+    dr=/media/gab2/$UUID
+    chl_vol="$vol_str$child_vol"
+    prn_vol="$vol_str$parent_vol"
+    fl=
+    pr_chk=$(btrs list $dr | grep "$parent_vol" )
+    ch_chk=$(btrs list $dr | grep "$child_vol" )
+    if [[ -n "$pr_chk" && -z "$ch_chk" ]]; then
+        echo "btr send -p $prn_vol $chl_vol | btr receive $dr"
+        btr send -p "$prn_vol" "$chl_vol" | btr receive $dr && \
+            echo "Child vol: \"$chl_vol\" of parent vol: \"$prn_vol\" sent to '$dr'" || \
+                fl=0
+    elif [[ -n "$pr_chk" && -n "$ch_chk" ]]; then
+        echo both volumes found - nothing to do
+    fi
+    [ -z "$fl" ] && umountLuksDev $dvc gab2 || echo "backup failed - check device $dvc for issues"
 fi
