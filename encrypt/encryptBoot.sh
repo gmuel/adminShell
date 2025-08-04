e3caaed30c3787dbd0d73aba42e51c5299298490
gmuel <gabmueller@hotmail.com>
2025-08-04 19:10:23 +0200

    [luks/mount01] comments added and minor corrections

--- encrypt/encryptBoot.sh
+++ encrypt/encryptBoot.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 helptxt(){
   cat << EOF
-    $0 BOOT_DVC [FS_TYPE]
-      Encryption facility script
+    $0 [options] BOOT_DVC [FS_TYPE]
+      Boot device encryption source script
       
  Usage:
-      createLuks1Boot:
+      createLuks1Boot
       
         Copies BOOT_DVC to local folder, encrypts BOOT_DVC as luks1
         with copies the original content back to said device
         on file system FS_TYPE (default: ext4)
         NOTE - the key file used will be automatically created 
         
+        Options       -h/--help print this message
+        
         BOOT_DEVICE - block device containing the boot partition (e.g. /dev/sda1, /dev/nvme1np1 )
         FS_TYPE     - file system type of the boot partition, NOTE should match the entry in /etc/fstab
       
         ATTENTION:  this will replace your previous boot partition, any errors can render your system
-                    unbootable. 
+                    unbootable. A backup file is created called /tmp/boot.tar. The rollback requires
+                    this file, the original boot device (first arg) and file system type
       
 EOF
   [ -z "$1" ] && return 0 || return $1
@@ -37,10 +40,10 @@ mountBoot(){
   [ -n "$1" ] && mount -v /boot/efi
 }
 mkfsAndCopy(){
-  uuid=$1
-  fs=$2
+  fs=$1
+  dvc=$2
   [ -z "$fs" ] && fs=ext4
-  mkfs.$fs -m0 /dev/mapper/boot_crypt
+  mkfs.$fs -m0 $dvc
   refreshFSTab $3
   mountBoot
   tar -C /boot --acls --xattrs -xf /tmp/boot.tar
@@ -71,7 +74,7 @@ createLuks1Boot(){
     runCmd cryptsetup luksAddKey $btDvc $kyfl && \
     runCmd addKey && \
     runCmd cryptdisks_start boot_crypt && \
-    runCmd mkfsAndCopy $uuid $fs && \
+    runCmd mkfsAndCopy $fs /dev/mapper/boot_crypt && \
     runCmd updateInitNGrub
 #  echo install -m0600 /dev/null /tmp/boot.tar
 #  install -m0600 /dev/null /tmp/boot.tar
@@ -108,17 +111,25 @@ addKey(){
 }
 
 refreshFSTab(){
-    [[  "$1" == "rollback" ]] && uuid1=$(blkid -s UUID -o value /dev/mapper/boot_crypt ) || uuid1=$((blkid -s UUID -o value $btDvc ))
+    [[  "$1" == "rollback" ]] && uuid1=$(blkid -s UUID -o value $btDvc ) || uuid1=$(blkid -s UUID -o value /dev/mapper/boot_crypt )
     sed -i "s/\(${btDvc//'/'/'\/'}\|UUID=[a-f0-9\-]\+\)\( \/boot .\+\)/UUID=$uuid1\2/g" /etc/fstab
 }
 
 rollback(){
-  uuid=$1
-  fs=$2
-  [ -z "$fs" ] && fs=ext4  
+  if [[ "$1" == "-h" || "$1" == "--help" ]]; then
+    cat << EOH
+   rollback - rollback encryption process, unmounts newly create boot and encrypts it,
+              writes new FS to boot device (btDvc, aka original device), updates fstab
+              and removes the boot entry from crypttab, finally initramfs and grub is called
+              
+              comment steps that are unnecessary in your rollback scenario 
+    
+EOH
+    return 0
+  fi
   umountBoot
   cryptdisks_stop boot_crypt
-  mkfsAndCopy $uuid $fs rollback
+  mkfsAndCopy $fs $btDvc rollback
   removeKey
   updateInitNGrub
 }
