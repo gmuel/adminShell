d27d28c6b7311609d8fd302d00797332cca47e5a
gmuel <gabmueller@hotmail.com>
2025-08-11 06:20:29 +0200

    [luks/mount01] bug fixes for fedora et. al sys, still ongoing

--- encrypt/encryptBoot.sh
+++ encrypt/encryptBoot.sh
@@ -20,14 +20,15 @@ helptxt(){
         ATTENTION:  this will replace your previous boot partition, any errors can render your system
                     unbootable. A backup file is created called /tmp/boot.tar. The rollback requires
                     this file, the original boot device (first arg) and file system type
+                    NOTE that 'inxi' command is required for this script to work, install before usage(!)
       
 EOF
   [ -z "$1" ] && return 0 || return $1
 }
 [[ "$1" == "-h" || "$1" == "--help" ]] && helptxt 0
-btDvc=$1
+[ -n "$1" ] && btDvc=$1 || btDvc=$(mount | grep '/boot ' | cut -d' ' -f1 )
 # kyfl=$2
-[ -n "$2" ] && fs=$2 || fs=ext4
+[ -n "$2" ] && fs=$2 || fs=$(grep '/boot ' /etc/fstab | sed "s/\s\+/ /g" | cut -d' ' -f3 )
 umountBoot(){
   if mount | grep "/boot " ; then
     mount | grep "/boot/efi " && umount /boot/efi
@@ -54,28 +55,48 @@ mkfsAndCopy(){
 }
 updateInitNGrub(){
   mount -a
-  if inxi -S | grep -i "\(ubuntu\|debian\)"; then  
+  if inxi -S | grep -i "\(linux mint\|ubuntu\|debian\)"; then  
     update-initramfs -u -k all
     update-grub
     grub-install
   elif inxi -S | grep -i "\(fedora\|centos\)"; then
-    echo "install_items+= /root/.keys/.*.key" | tee -a /etc/dracut.conf.d/cryptodisk.conf
-    dracut -vf --regenerate-all
-    echo GRUB_ENABLE_CRYPTODISK=y | tee -a /etc/ddefault/grub
-    grub2-mkconfig -o /boot/grub2/grub.cfg #"$(readlink -e /etc/grub-efi.cfg )"
+    grep "^GRUB_ENABLE_CRYPTODISK=y" /etc/default/grub || echo GRUB_ENABLE_CRYPTODISK=y | tee -a /etc/default/grub
+    grub2-mkconfig -o /boot/grub2/grub.cfg
+    cdf=/etc/dracut.conf.d/cryptodisk.conf
+    grep "/etc/cryptsetup-keys.d" $cdf || echo "install_items+=\" /etc/cryptsetup-keys.d/* \"" | tee -a $cdf
+    dracut -vf --regenerate-all -p
 #    uuid1=$(blkid -s UUID -o value /dev/mapper/boot_crypt )
 #    uuid2=${uuid1//'-'/''}
 #    sed "s/\(search \-\-no\-floppy \-\-fs\-uuid \-\-set=dev \)\($uuid\)/cryptomount -u $uuid2\n\1\-\-hint='cryptouuid=/$uuid2' \2" /boot/efi/EFI/fedora/grub.cfg
 #    dnf reinstall shim-\* grub2-efi-\* grub2-common -y
   else
-    uname -a 
+    inxi -S 
     echo system not supported - update initramfs and grub manually
     return -1
   fi
 
 }
+createKeyFile(){
+  if inxi -S | grep -i "\(fedora\|centos\)"; then
+    kyfl=/etc/cryptsetup-keys.d/luks-${uuid}.key
+  elif inxi -S | grep -i "\(linux mint\|debian\|ubuntu\)"; then
+    kyfl=/root/.keys/.${uuid}.key
+  fi
+  runCmd dd if=/dev/urandom of=$kyfl bs=512 count=8
+}
 includeKey(){
   sed -i.1 "s/\(UUID=$1\) none \(luks\(,discard\)\?\)/\1 ${kyfl//'/'/'\/'} \2/g" /etc/crypttab
+}
+uuid=
+trgt=
+decryptBoot(){
+  if inxi -S | grep -i "\(fedora\|centos\)"; then
+    trgt=luks-$uuid
+  elif inxi -S | grep -i "\(linux mint\|debian\|ubuntu\)"; then
+    trgt=boot_crypt
+  fi
+  runCmd cryptsetup luksOpen $btDvc $trgt --key-file $kyfl && \
+    runCmd mkfsAndCopy $fs /dev/mapper/$trgt
 }
 createLuks1Boot(){
   #echo   mount -o remount,ro /boot
@@ -88,13 +109,11 @@ createLuks1Boot(){
     runCmd dd if=/dev/urandom of=\$btDvc bs=1M status=none # && \
     runCmd cryptsetup luksFormat --type luks1 $btDvc && \
     runCmd uuid=$(blkid -o value -s UUID $btDvc) && \
-    kyfl=/root/.keys/.${uuid}.key && \
-    runCmd dd if=/dev/urandom of=$kyfl bs=512 count=8 && \
+    runCmd createKeyFile && \
     runCmd chmod 0600 $kyfl && \
     runCmd cryptsetup luksAddKey $btDvc $kyfl && \
     runCmd addKey && \
-    runCmd cryptsetup luksOpen $btDvc boot_crypt --key-file $kyfl && \
-    runCmd mkfsAndCopy $fs /dev/mapper/boot_crypt && \
+    runCmd decryptBoot && \
     runCmd updateInitNGrub
 #  echo install -m0600 /dev/null /tmp/boot.tar
 #  install -m0600 /dev/null /tmp/boot.tar
@@ -123,16 +142,23 @@ runCmd(){
     eval $@
 }
 removeKey(){
-    grep ^boot_crypt /etc/crypttab && sed -i "s/^\(boot_crypt UUID=[a-f0-9\-]\+.\+\)/\# \1 - archived on '$(date +%Y%m%d )'/g" /etc/crypttab
+    grep ^$trgt /etc/crypttab && sed -i "s/^\($trgt UUID=[a-f0-9\-]\+.\+\)/\# \1 - archived on '$(date +%Y%m%d )'/g" /etc/crypttab
 }
 addKey(){
     removeKey
-    echo boot_crypt UUID=$uuid $kyfl luks,discard,key-slot=1 | tee -a /etc/crypttab
+    addSlot=
+    if inxi -S | grep -i "\(linux mint\|debian\|ubuntu\)"; then
+      addSlot=,key-slot=1
+    fi
+
+    echo $trgt UUID=$uuid $kyfl luks,discard$addSlot | tee -a /etc/crypttab
 }
 
 refreshFSTab(){
-    [[  "$1" == "rollback" ]] && uuid1=$(blkid -s UUID -o value $btDvc ) || uuid1=$(blkid -s UUID -o value /dev/mapper/boot_crypt )
-    sed -i "s/\(${btDvc//'/'/'\/'}\|UUID=[a-f0-9\-]\+\)\( \/boot .\+\)/UUID=$uuid1\2/g" /etc/fstab
+    uuid1=
+    [[ "$1" == "rollback" ]] && uuid1=$(blkid -s UUID -o value $btDvc ) || uuid1=$(blkid -s UUID -o value /dev/mapper/$trgt )
+    sed -i "s/\(${btDvc//'/'/'\/'}\|UUID=$uuid\+\)\( \/boot .\+\)/UUID=$uuid1\2/g" /etc/fstab
+    systemctl daemon-reload
 }
 
 rollback(){
@@ -148,7 +174,7 @@ EOH
     return 0
   fi
   umountBoot
-  cryptdisks_stop boot_crypt
+  cryptsetup luksClose $trgt
   mkfsAndCopy $fs $btDvc rollback
   removeKey
   updateInitNGrub
