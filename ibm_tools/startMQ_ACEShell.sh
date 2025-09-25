8708ed88646d9f4578865df19a09eb12c3db1a50
fun_gab <gabmueller@hotmail.com>
2025-09-25 14:32:25 +0200

    [luks/mount01] odbc env vars included

--- ibm_tools/startMQ_ACEShell.sh
+++ ibm_tools/startMQ_ACEShell.sh
@@ -3,6 +3,9 @@ MQ_HOME=/opt/mqm/bin
 ACE_HOME=/opt/ace-12.0.5.0/server/bin
 
 export PATH=${MQ_HOME}:${ACE_HOME}:$PATH
+export ODBCSYSINI=/var/mqsi/odbc
+export DB2CLIINIPATH=$ODBCSYSINI
+export ODBCINI=$ODBCSYSINI/odbc.ini
 
 . mqsiprofile
 
