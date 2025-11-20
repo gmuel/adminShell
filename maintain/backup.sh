f8a1217d0ea742a7db3fa9b492f92a486cc6c59c
fun_gab <gabmueller@hotmail.com>
2025-11-20 15:59:51 +0100

    [luks/mount01] minor subscript fix and recursive umount issue fix

--- maintain/backup.sh
+++ maintain/backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-vol_str=$1
-if [[ "$vol_str" == "-h" || "$vol_str" == "--help" ]]; then
+
+helptext(){
     cat << EOH
 $0 [options] [arg] - create backup arg/@date_string for any mount btrfs fs
         
@@ -12,48 +12,83 @@ $0 [options] [arg] - create backup arg/@date_string for any mount btrfs fs
             1  - no mount point
             -1 - no matching source device or parent volume found
             -2 - decryption/mount of destination device failed
+			-3 - no snapshot created on given local subvol
 
 EOH
-    exit 0
+}
+
+allUUIDs(){
+	str=
+	for i in $(cut -d' ' -f1 backup.map ); do
+		str="$str\\\|$i"
+	done
+	echo $str
+}
+declare -a args=( $@ )
+vol_str=${args[0]}
+
+if [[ "$vol_str" == "-h" || "$vol_str" == "--help" ]]; then
+	helptext && exit 0
 fi
-source /home/gab2/bin/mountLuksDev.sh >> /dev/null
+
+if [ -z "$backvol" ]; then
+	tmp_fl=/tmp/.inxi-M.out
+	[ ! -f $tmp_fl ] && inxi -M >> $tmp_fl
+	if grep XPS $tmp_fl; then
+    	backvol=2
+	elif grep "\(VirtualBox\|QEMU\)" $tmp_fl
+		backvol=3
+	else
+    	backvol=1
+	fi
+	export backvol=$backvol
+	export PATH=/home/gab2/bin:$PATH
+fi
+echo "backup subvol suffix found: $backvol"
+
+
+. mountLuksDev.sh >> /dev/null
+
 [ -z "$vol_str" ] && vol_str=/home
+
 len=${#vol_str}
+
 [[ "${vol_str:$((len-1))}" != "/" ]] && vol_str="$vol_str/"
+
 if [ ! -d $vol_str ]; then
     echo no such mount point $vol_str
     exit 1
 fi
+
 cd $vol_str
 dt_str=$(date +%Y%m%d)
 parent_vol=$(btrs list ./ | grep -v "@$dt_str" | grep "@20\(2[4-9]\|[3-9][0-9]\)" | tail -1 | cut -d' ' -f9 )
+
 if [[ "$?" != "0" || -z "$parent_vol" ]]; then
     echo "No suitable subvol found for parent in $vol_str"
     exit -1
 fi
+
 echo Found parent volume "$vol_str$parent_vol"
+
 child_vol=$(btrs list ./ | grep "@$dt_str" | tail -1 | cut -d' ' -f9 | sed "s/\/\?\(.\+\)/\/\1/g" | grep ".\+")
-if [ -z "$child_vol" ]; then
-     child_vol=$(btrs snapshot -r $vol_str "@$dt_str" | grep -q "Create" && echo "@$dt_str" )
-    echo "and new child volume $vol_str$child_vol created"
+
+if [ -z "$child_vol" ]; then # no child subvol -> create new snapshot,
+    child_vol=$(btrs snapshot -r $vol_str "@$dt_str" | grep -q "Create" && echo "@$dt_str" )
+    [ -z "$child_vol" ] && echo snapshot creation failed - check dmesg/journal for more info && exit -3
+	echo "and new child volume $vol_str$child_vol created"
 else
     child_vol=$(echo $child_vol | sed "s/\/\(.\+\)/\1/g" )
     echo "and child volume found: $vol_str$child_vol"
 fi
-backvol=
-if inxi -M | grep XPS; then
-    backvol=2
-else
-    backvol=1
-fi
-echo "backup subvol suffix found: $backvol"
+
 UUID=
 sbvl=
-dvc=$(blkid | grep LUKS | grep "\(dac895e7-151b-4894-98b1-5d7165ff8c76\|62bc1b28-ab06-4e28-8167-2fe0e3c9499d\)" | sed "s/\(\/dev\/sd[a-z][1-9]\).\+/\1/g" )
+dvc=$(blkid | grep LUKS | grep "\($(allUUIDs )\)" | sed "s/\(\/dev\/sd[a-z][1-9]\).\+/\1/g" )
 echo $dvc used as backup
 UUID=$(blkid -s UUID -o value $dvc )
 echo having UUID $UUID
-sbvl="$(grep $UUID /home/gab2/bin/backup.map | cut -d' ' -f2 )$backvol"
+sbvl="$(grep $UUID backup.map | cut -d' ' -f2 )$backvol"
 if echo $vol_str | grep -v home; then
     ssvl=
     if echo $vol_str | grep var; then
@@ -80,19 +115,39 @@ if [ ! -z "$sbvl" ]; then
     chl_vol="$vol_str$child_vol"
     prn_vol="$vol_str$parent_vol"
     fl=
+	ext=0
     pr_chk=$(btrs list $dr | cut -d' ' -f9 | grep "^$parent_vol" )
     ch_chk=$(btrs list $dr | cut -d' ' -f9 | grep "^$child_vol" )
     if [[ -n "$pr_chk" && -z "$ch_chk" ]]; then
         echo "btr send -p $prn_vol $chl_vol | btr receive $dr"
-        btr send -p "$prn_vol" "$chl_vol" | btr receive $dr && \
-            echo "Child vol: \"$chl_vol\" of parent vol: \"$prn_vol\" sent to '$dr'" || \
-                fl=0
+        if ! btr send -p "$prn_vol" "$chl_vol" | btr receive $dr; then
+			echo "Child vol: \"$chl_vol\" of parent vol: \"$prn_vol\" sent to '$dr'"
+			ext=-4            
+			fl=fail
+		fi
     elif [[ -n "$pr_chk" && -n "$ch_chk" ]]; then
         echo both volumes found - nothing to do
     fi
-    declare -a args=( $@ )
-    if [[ ${#args[@]} != 0 ]]; then
-        $0 ${args[@:1:]}
+	sz=${#args[@]}
+    if [[ "$sz" != "0" ]]; then
+		if [ -z "$BACK_UP_REC_CALL" ]; then
+			export BACK_UP_REC_CALL=1
+		else
+			fl=rec_call
+		fi
+        if [[ "$sz" > "1" ]]; then
+			if ! $0 ${args[@]:1}; then
+				ext=$?
+				fl=fail_rec
+			fi
+		fi
     fi
-    [ -z "$fl" ] && umountLuksDev $dvc gab2 || echo "backup failed - check device $dvc for issues"
+    if [[ -z "$fl" ]]; then
+		umountLuksDev $dvc gab2
+	elif [[ "$fl" == "rec_call" ]]; then
+		# skip
+	else
+		echo "backup failed with flag '$fl' - check device $dvc for issues"
+		exit $ext
+	fi
 fi
