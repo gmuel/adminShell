39ed840ffe83a8bcb07401a38da9293dbc4e7431
Gabriel Mueller <gabmueller@hotmail.com>
2025-12-11 12:32:00 +0100

    [luks/mount01] new setup script for nvidia specific service units needed to hibernate/suspend-2-disk

---
+++ maintain/nvidia-hibernate-setup.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+helptxt(){
+    cat << EOH
+$0 [options]
+    create nvidia driver required service units for hibernation/suspend-2-disk
+
+    options:
+        -h/--help   print this message
+EOH
+}
+
+fl=/etc/systemd/system/nvidia-hibernate.service
+
+if [ ! -f $fl ]; then
+    cat << EOI >> $fl
+[Unit]
+Description=NVIDIA system hibernate actions
+Before=systemd-hibernate.service
+
+[Service]
+Type=oneshot
+ExecStart=/usr/bin/logger -t hibernate -s "nvidia-hibernate.service"
+ExecStart=/usr/bin/nvidia-sleep.sh "hibernate"
+
+[Install]
+RequiredBy=systemd-hibernate.service
+EOI
+
+fi
+
+fl=/etc/systemd/system/nvidia-suspend.service
+
+if [ ! -f $fl ]; then
+    cat << EOI >> $fl
+[Unit]
+Description=NVIDIA system suspend actions
+Before=systemd-suspend.service
+
+[Service]
+Type=oneshot
+ExecStart=/usr/bin/logger -t suspend -s "nvidia-suspend.service"
+ExecStart=/usr/bin/nvidia-sleep.sh "suspend"
+
+[Install]
+RequiredBy=systemd-suspend.service
+EOI
+
+fi
+
+fl=/etc/systemd/system/nvidia-resume.service
+
+if [ ! -f $fl ]; then
+    cat << EOI >> $fl
+[Unit]
+Description=NVIDIA system resume actions
+After=systemd-suspend.service
+After=systemd-hibernate.service
+
+[Service]
+Type=oneshot
+ExecStart=/usr/bin/logger -t suspend -s "nvidia-resume.service"
+ExecStart=/usr/bin/nvidia-sleep.sh "resume"
+
+[Install]
+RequiredBy=systemd-suspend.service
+RequiredBy=systemd-hibernate.service
+EOI
+
+fi
+
+fl=/lib/systemd/system-sleep/nvidia
+if [ -f $fl ] && grep -v -q 'pre)' $fl ; then
+    sed -i.1 "s/\(post)\)/pre)\n        \/usr\/bin\/nvidia-sleep\.sh \"suspend\"\n    \1/g" /lib/systemd/system-sleep/nvidia
+fi
