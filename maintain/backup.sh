1a8e9469d74505f61bf95c612748a63f1bd12222
gab2 <gabmueller@hotmail.com>
2025-12-31 20:05:53 +0100

    [luks/mount] parent/child snaps filtering corrected

--- maintain/backup.sh
+++ maintain/backup.sh
@@ -26,13 +26,13 @@ if [ ! -d $vol_str ]; then
 fi
 cd $vol_str
 dt_str=$(date +%Y%m%d)
-parent_vol=$(btrs list ./ | grep -v "^@$dt_str" | grep "@20\(2[4-9]\|[3-9][0-9]\)" | tail -1 | cut -d' ' -f9 )
+parent_vol=$(btrs list ./ | grep -v "@$dt_str" | grep "\s\+@20\(2[4-9]\|[3-9][0-9]\)" | tail -1 | cut -d' ' -f9 )
 if [[ "$?" != "0" || -z "$parent_vol" ]]; then
     echo "No suitable subvol found for parent in $vol_str"
     exit -1
 fi
 echo Found parent volume "$vol_str$parent_vol"
-child_vol=$(btrs list ./ | grep "@$dt_str" | tail -1 | cut -d' ' -f9 | sed "s/\/\?\(.\+\)/\/\1/g" | grep ".\+")
+child_vol=$(btrs list ./ | grep "\s\+@$dt_str" | tail -1 | cut -d' ' -f9 | sed "s/\/\?\(.\+\)/\/\1/g" | grep ".\+")
 if [ -z "$child_vol" ]; then
      child_vol=$(btrs snapshot -r $vol_str "@$dt_str" | grep -q "Create" && echo "@$dt_str" )
     echo "and new child volume $vol_str$child_vol created"
