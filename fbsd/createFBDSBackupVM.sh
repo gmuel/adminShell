72850f437c7e8a4de5e40b1de7d0228213ec6606
fun_gab <gabmueller@hotmail.com>
2026-03-04 15:32:55 +0100

    [luks/mount01] 'append' mode completed/tested

--- fbsd/createFBDSBackupVM.sh
+++ fbsd/createFBDSBackupVM.sh
@@ -12,6 +12,7 @@ pool_name=
 back_pool=
 snap_name=
 app_part=1
+swap_sz=
 
 ERR_DEVC=1
 ERR_BACK=2
@@ -29,28 +30,36 @@ computeZFS(){
 computeZFSSize(){
 	computeZFS $1 2
 }
-
+findPartByLabel(){
+	gpart show -l $1 | grep $2 | awk '{print $3}'
+}
 lastPartId(){
 	gpart show $1 | grep -v "\($1\|\- free \-\)" | grep "\S\{1,\}" | tail -1 | awk '{print $3}'
 }
 
 createPartTable(){
-    gpart create -s GPT $dvc
-	gpart add -t efi -l efiboot0 -b 40 -s 260M $dvc
+    echo gpart create -s GPT $dvc
+	echo gpart add -t efi -l efiboot0 -b 40 -s 260M $dvc
 }
 partStart(){
     computeZFS $1 1
 }
+createPartIFNEXT(){
+	if ! gpart show -l $1 | grep $3 ; then
+		part_start=$(partStart $1 )
+		[ -n "$5" ] && part_start=$(($part_start+$5))
+		gpart add -t $4 -l $3 -b $part_start -s $2 $1
+	fi
+}
 addPartFBSD(){
 	p_id=2
-	p_start=0
 	if [ "$app_part" = "0" ]; then
-		p_id=$((1+$(lastPartId $dvc )))
-		p_id=$(partStart $dvc )
+		p_id=$(findPartByLabel $dvc gptboot0 )
+		[ -z "$p_id" ] && p_id=$((1+$(lastPartId $dvc )))
 	fi
-    gpart add -t freebsd-boot -l gptboot0 -b $((532520+$p_start)) -s 1024 $dvc
-    gpart add -t freebsd-swap -l swap0 -b $((534528+$p_start)) -s 2G $dvc
-    gpart add -t freebsd-zfs -l zfs0 -b $((4728832+$p_start)) -s $(($(computeZFSSize $dvc )-2008)) $dvc
+	createPartIFNEXT $dvc 1024 gptboot0 freebsd-boot
+    createPartIFNEXT $dvc $swap_sz swap0 freebsd-swap 984
+    createPartIFNEXT $dvc $(($(computeZFSSize $dvc )-2008)) zfs0 freebsd-zfs
     gpart bootcode -b /boot/pmbr -p /boot/gptzfsboot -i $p_id $dvc
 
 }
@@ -62,12 +71,18 @@ listZFSFSs(){
 	done
 }
 createPool(){
-	[ "$app_part" = "1" ] && p_id=4 || p_id=$((3+$(lastPartId )))
+	if [ "$app_part" = "1" ]; then
+		p_id=4
+	else
+		p_id=$(findPartByLabel $dvc zfs0 )
+#		[ -z "$p_id" ] && p_id=$((3+$(lastPartId $dvc )))
+	fi
     zpool create $pool_name /dev/${dvc}p$p_id
 }
 copyBackup(){
+	bu=$(echo $backup | sed 's=/=\\/=g' )
     for i in $(listZFSFSs $back_pool NAME snapshot | grep $snap_name ); do
-	    trg=$(echo $i | sed "s/$backup//g" | sed 's/@.\{1,\}//g' )
+	    trg=$(echo $i | sed "s/$bu//g" | sed 's/@.\{1,\}//g' )
 	    zfs send $i | zfs receive $pool_name/root$trg
     done
 }
@@ -82,16 +97,25 @@ correctUSR(){
 adjustMounts(){
     for i in $(listZFSFSs $pool_name "\($pool_name\$\|NAME\)" | sort -r ); do
         if [ "$i" = "$pool_name/root/ROOT/default" ]; then
-            zfs set mountpoint=/ $i
+            zfs set -u mountpoint=/ $i
         else
-            zfs set mountpoint=$(echo $i | sed "s/$pool_name\/root//g" ) $i
+            zfs set -u mountpoint=$(echo $i | sed "s=$pool_name/root==g" ) $i
         fi
     done
 
 }
 
 prepareEFI(){
-    [ "app_part" = "1" ] && newfs_msdos -F 32 -c 1 /dev/${dvc}p1
+    if [ "app_part" = "1" ]; then
+		newfs_msdos -F 32 -c 1 /dev/${dvc}p1
+	else
+		fs_tab=/$pool_name/root/ROOT/default/etc/fstab
+		# correct efi entry
+		sed -i'' -e "s=$(grep efi $fs_tab | awk '{print $1}' )=/dev/${dvc}p1=g" $fs_tab
+		# correct swap entry
+		sed -i'' -e "s=$(grep swap $fs_tab | awk '{print $1}' )=/dev/${dvc}p$(findPartByLabel $dvc swap0 )=g" $fs_tab
+		
+	fi
     mount -t msdosfs /dev/${dvc}p1 /mnt
 	dr=/mnt/efi
     [ ! -d $dr/boot ] && mkdir -p /mnt/efi/boot
@@ -110,7 +134,7 @@ umountClone(){
 
 helptxt(){
     cat << EOH
- $0 [Option] DEVICE BACKUP-POOL SNAP-SHOT [NEW-POOL]
+ $0 [Option] DEVICE BACKUP-POOL SNAP-SHOT [NEW-POOL] [SWAP-SIZE]
  
  	1) sets up a new GPT type disk with zfs as fourth partition, or
 
@@ -129,6 +153,7 @@ helptxt(){
         BACKUP-POOL zfs backup dataset to recover, e.g. backup_pool/data
 	SNAP-SHOT   backup snapshot name, e.g. @recent, @20250101,...
         NEW-POOL    zpool backup being newly create, default is zclone
+        SWAP-SIZE   swap size string, e.g. 512M, 4G, ..., defaults to 2G
  Options:
         -h/--help   print this message
 	-a/--append only append new FreeBSD partitions,
@@ -143,7 +168,7 @@ helptxt(){
 		$ERR_SNAP - backup clone failed
 		$ERR_CUSR - correct /usr failed
 		$ERR_MOUT - mount adjustments failed
-		$ERR_PEFI - prepping EFI system partition failed
+		$ERR_PEFI - prepping EFI system partition or adjusting fstab failed
 		
 EOH
 }
@@ -151,18 +176,19 @@ main(){
     case $1 in
     '-h'|'--help') helptxt && return 0
     ;;
-	'-a,--append') app_part=0
+	'-a'|'--append') app_part=0
 	;;
     esac
 	shift
     dvc=$1
     backup=$2
-	snap_shot=$3
+	snap_name=$3
 	back_pool=$(echo $backup | cut -d/ -f1 )
     pool_name=${4:-zclone}
+	swap_sz=${5:-2G}
     [ -z "$dvc" ] && echo no root block device given - aborting && return $ERR_DEVC
     [ -z "$backup" ] && echo no backup pool given - aborting && return $ERR_BACK
-    [ -z "$snap_shot" ] && echo no backup snapshot given - aborting && return $ERR_NOSN
+    [ -z "$snap_name" ] && echo no backup snapshot given - aborting && return $ERR_NOSN
     
 	if [ "$app_part" = "1" ]; then
 		createPartTable || return $ERR_PART
