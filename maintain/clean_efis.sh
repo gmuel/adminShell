7d28ff27c626707fbeffac851739b877b8b9be28
gab2 <gabmueller@hotmail.com>
2026-05-08 18:25:35 +0200

    [luks/mount02] wider kver range included, less verbose

--- maintain/clean_efis.sh
+++ maintain/clean_efis.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-# listing currently installed kernels
-declare -a krnls=( $(apt list --installed 2>> /dev/null \
-    | grep ^linux | cut -d' ' -f2 | sort | uniq | grep "^6\(\.[0-9]\+\)\{2\}" \
-        | sed "s/\(6\(\.[0-9]\+\)\{2\}\-[0-9]\+\).\+/\1/g" ) )
+# listing currently installed kernel version ids
+declare -a krnls=( $(ls /boot/vmlinuz-* \
+        | sed "s/.\+\([5-7]\(\.[0-9]\+\)\{2\}\-[0-9]\+\).\+/\1/g" ) )
 
 # create OR pattern
 klst=$(echo ${krnls[@]} | sed "s/\s/\\\|/g" )
 
 # run through all efi files and discard all missing in currently installed kernel list
 for i in $(ls /boot/efi/EFI/Linux/*.efi ); do
-    if ! echo $i | grep "\($klst\)"; then
+    if ! echo $i | grep -q "\($klst\)"; then
         mv $i /boot/.dump
     fi
 done
