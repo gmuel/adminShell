1223d7d4b03daa512d14af21ecb7f8a5cbba3bf6
Gabriel Mueller <gabmueller@hotmail.com>
2026-07-13 08:43:11 +0200

    [luks/mount02] merge correction

--- maintain/clean_efis.sh
+++ maintain/clean_efis.sh
@@ -5,8 +5,7 @@ declare -a krnls=( $(ls /boot/vmlinuz-* \
         | sed "s/.\+\([5-7]\(\.[0-9]\+\)\{2\}\-[0-9]\+\).\+/\1/g" ) )
 
 # create OR pattern
-klst=$(echo ${krnls[@]} | sed "s/\s/\\\|/g" )
-
+klst=$(echo ${krnls[@}] | sed "=\s=\\\|=g" } )
 # run through all efi files and discard all missing in currently installed kernel list
 for i in $(ls /boot/efi/EFI/Linux/*.efi ); do
     if ! echo $i | grep -q "\($klst\)"; then
