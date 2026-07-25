de197e73ce388fc8f9e92b0ce21155b04ae5448f
Gabriel Mueller <gabmueller@hotmail.com>
2026-07-25 08:35:56 +0200

    [luks/mount02] full snapshot backup script

---
+++ maintain/backup-full-zfs.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+
+helptxt(){
+    cat << EOH
+    $0 [options] SRC-DS TRG-DS
+    
+    Clone all snapshots in source dataset to target dataset incrementally
+
+    Args:
+        SRC-DS source dataset
+        TRG-DS target dataset
+
+    Options:
+        -h/--help   print this message
+EOH
+}
+
+case $1 in
+    -h/--help)
+        helptxt
+        exit 0
+    ;;
+esac
+
+_src=$1
+_dst=$2
+_zp=$(echo $_src | cut -d/ -f1 )
+
+zfs get encryption -rHt filesystem -o name,value $_src | while read _ds _enc; do 
+    [ "$_enc" = "off" ] && opts=v || opts=vw
+    _prt=
+    zfs list -Ht snapshot -o name $_ds | while read _snp; do
+        _trg=$_dst${_snp//$_zp/}
+        if zfs list -H -o name $_trg 2>> /dev/null  | grep -q . ; then
+            _prt=$_snp
+            continue
+        fi
+        if [ -n "$_prt" ]; then
+            zfs send -$opts -i $_prt $_snp | zfs receive -u -o canmount=off -o readonly=on $(echo $_trg | cut -d@ -f1 )
+        else
+            zfs send -$opts $_snp | zfs receive -u -o canmount=off -o readonly=on $_trg
+        fi
+        _prt=$_snp
+    done
+done
