1bb5d422341a69fa05d0179b6bf0228733d3d03b
fun_gab <gabmueller@hotmail.com>
2026-08-11 07:21:45 +0200

    [luks/mount02] empty string test for aux function in kill-all

--- encrypt/killAll.sh
+++ encrypt/killAll.sh
@@ -2,7 +2,7 @@
 set -x
 getChildPIDs(){
     _dpt=${2:-0}
-	ps -ef | grep $1 | awk '{print $2}' | grep -v $1 | sort -r | grep "[0-9]\+" | while read _cid; do
+    [ -n "$1" ] && ps -ef | grep $1 | awk '{print $2}' | grep -v $1 | sort -r | grep "[0-9]\+" | while read _cid; do
 		[ $_dpt -le 3 ] && getChildPIDs $_cid $(($_dpt+1))
 		echo $_cid
 	done
