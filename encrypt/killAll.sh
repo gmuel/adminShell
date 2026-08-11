52964b697a7d48c4286b2b007001caf631c1163d
fun_gab <gabmueller@hotmail.com>
2026-08-11 06:56:56 +0200

    [luks/mount02] minor kill-all fixes

--- encrypt/killAll.sh
+++ encrypt/killAll.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 getChildPIDs(){
-	ps -ef | grep $1 | awk '{print $2}' | grep -v $1 | sort -r | while read _cid; do
+	ps -ef | grep $1 | awk '{print $2}' | grep -v $1 | sort -r | grep "[0-9]\+" | while read _cid; do
 		getChildPIDs $_cid
 		echo $_cid
 	done
 }
+
+if [ -z "$1" ] || [ ! -d /home/$1 ] || ! mount | grep $1; then
+	exit
+fi
 
 ps -u $1 | awk '{print $1}' | sort -r | while read _pid; do
 	getChildPIDs $_pid | while read _cid; do
