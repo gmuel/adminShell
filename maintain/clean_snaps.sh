ff68c64c474e62e0e0d950ad5871e09f3fa9953b
Gabriel Mueller <gabmueller@hotmail.com>
2026-09-08 15:33:32 +0200

    [luks/mount02] zfs snapshot clean script

---
+++ maintain/clean_snaps.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+_cnt=${1:-"-2"}
+zfs list -rt filesystem rpool -Ho name | while read _ds; do
+	_excl="$(zfs list -t snapshot -Ho name $_ds | tail $_cnt )"
+	_excl="$(echo $_excl | sed "s=\s\+=\\|=g" )"
+	echo $_excl
+	zfs list -t snapshot -Ho name $_ds | grep -v "\($_excl\)" | while read _snp; do
+		zfs destroy $_snp
+	done
+done
