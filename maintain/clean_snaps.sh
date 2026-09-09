05d72b444307b4c8f31ec1eb5410695036686aa5
gab2 <gabmueller@hotmail.com>
2026-09-09 19:52:04 +0200

    [luks/mount02] fixed issue with missing backslash

--- maintain/clean_snaps.sh
+++ maintain/clean_snaps.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-
+set -x
 _cnt=${1:-"-2"}
 zfs list -rt filesystem rpool -Ho name | while read _ds; do
 	_excl="$(zfs list -t snapshot -Ho name $_ds | tail $_cnt )"
-	_excl="$(echo $_excl | sed "s=\s\+=\\|=g" )"
+	_excl="$(echo $_excl | sed "s=\s\+=\\\|=g" )"
 	echo $_excl
 	zfs list -t snapshot -Ho name $_ds | grep -v "\($_excl\)" | while read _snp; do
 		zfs destroy $_snp
