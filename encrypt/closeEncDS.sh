d6cbe12dbf6e23aa1ecdb49a4cc2d2efb24894ca
Gabriel Müller <gabmueller@hotmail.com>
2026-09-14 11:16:22 +0200

    [luks/mount02] generalized script for different pool names

--- encrypt/closeEncDS.sh
+++ encrypt/closeEncDS.sh
@@ -2,15 +2,26 @@
 
 set -x
 
+unmountDS(){
+    _ds=$1
+    if [ "$(zfs get mountpoint -Ho value $_ds )" = "legacy" ]; then
+        umount ${_ds//"$2"/}
+        zfs unload-key $_ds
+    else
+        zfs unmount -u $_ds
+    fi
+}
+
 [ -z "$1" ] && exit 0
 _dr=$(dirname $0 )
 [ "${_dr:0:1}" != "/" ] && _dr="$(pwd )/$_dr"
 echo $PATH | grep -q $_dr || export PATH=$_dr:$PATH
-_ds=rpool/home/$1
+_zp=$(zpool get name -Ho value )
+_ds=$_zp/home/$1
 mount | grep $_ds || exit 0
 who | grep $1 && exit 0
 _enc=$(zfs get encryption -Ho value $_ds 2>> /dev/null )
 
 if [ -n "$_enc" ] && [ $_enc != "off" ] ; then
-	killAll.sh $1 && zfs unmount -u $_ds
+	killAll.sh $1 && unmountDS $_ds $_zp
 fi
